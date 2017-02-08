cb145a3c73f3382dfdd6fe0d907a0d006fa02f69
jay Freeman and Philip Putnam <philip@>
2017-02-07 16:52:00 -0800

    setup twig pages and app.php for PHP-Places

---
+++ app/app.php
@@ -0,0 +1,33 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Place.php";
+
+    session_start();
+
+    if (empty($_SESSION['places'])) {
+      $_SESSION['places'] = array();
+    }
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views/'
+    ));
+
+    $app->get("/", function() use ($app) {
+
+        return $app['twig']->render('root.html.twig', array('places' => Place::getAll()));
+
+    });
+
+    $app->post("/add-places", function() use ($app) {
+        $place = new Place($_POST['place']);
+        $place->save();
+        return $app['twig']->render('add-places.html.twig');
+
+    });
+
+
+    return $app;
+?>
--- src/Place.php
+++ src/Place.php
@@ -3,8 +3,26 @@
     {
         private $city_name;
 
-        
+        function __construct($city_name) {
+            $this->city_name = $city_name;
+        }
+
+        function setCityName($new_name) {
+            $this->city_name = $new_name;
+        }
+
+        function getCityName() {
+            return $this->city_name;
+        }
+
+        function save()
+        {
+            array_push($_SESSION['places'], $this);
+        }
+
+        static function getAll()
+        {
+            return $_SESSION['places'];
+        }
     }
-
-
 ?>
